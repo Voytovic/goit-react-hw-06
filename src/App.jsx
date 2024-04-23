b8e765a60cf391eb23cb21c6d7be953f0f95d617
Voytovic <voytovic@gmail.com>
2024-04-23 19:45:34 +0300

    upd final

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import ContactList from './components/ContactList/ContactList';
-import SearchBox from './components/SearchBox/CearchBox';
+import SearchBox from './components/SearchBox/SearchBox';
 import ContactForm from './components/ContactsForm/ContactsForm';
 
 function App() {
